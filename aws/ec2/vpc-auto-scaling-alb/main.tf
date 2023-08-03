192396ae58359b99c4c09ae183f6d2f7c4cf21ca
Madhan <madhankumaravelu93@gmail.com>
2023-08-02 23:53:31 -0500

    feat: initial commit of vpc and asg

---
+++ aws/ec2/vpc-auto-scaling-alb/main.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.10.0"
+    }
+  }
+}
+
+module "my_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.1"
+
+  name = "my-vpc"
+  cidr = "10.10.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
+  public_subnets  = ["10.10.101.0/24", "10.10.102.0/24", "10.10.103.0/24"]
+
+  single_nat_gateway = true
+  create_igw = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+module "my_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "my security group"
+  description = "Security group for web server"
+  vpc_id      = module.my_vpc.vpc_id
+
+  ingress_cidr_blocks      = [module.my_vpc.vpc_cidr_block]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "http"
+      description = "Opened web server ports"
+      cidr_blocks = module.my_vpc.vpc_cidr_block
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "ssh"
+      description = "ssh port"
+      cidr_blocks = module.my_vpc.vpc_cidr_block
+    }
+  ]
+}
+
+data "aws_ami" "amazon-linux-image" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+module "my_asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.10.0"
+
+  name = "my auto scaling group"
+
+  min_size                  = 2
+  max_size                  = 4
+  desired_capacity          = 2
+  wait_for_capacity_timeout = 0
+  health_check_type         = "EC2"
+  vpc_zone_identifier       = module.my_vpc.private_subnets
+
+  # Launch template
+  launch_template_name        = "my-launch-template"
+  launch_template_description = "Launch template"
+  update_default_version      = true
+
+  image_id          = data.aws_ami.amazon-linux-image.id
+  instance_type     = "t3.micro"
+  ebs_optimized     = true
+  enable_monitoring = true
+}
