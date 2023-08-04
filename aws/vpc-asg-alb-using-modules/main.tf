23afc6e8bb454f9e3d01d276b4b98ae97d862618
Madhan <madhankumaravelu93@gmail.com>
2023-08-04 11:16:54 -0500

    chore: folder re-structure

--- aws/vpc-asg-alb-using-modules/main.tf
+++ aws/vpc-asg-alb-using-modules/main.tf
@@ -14,7 +14,7 @@ module "my_vpc" {
   name = "my-vpc"
   cidr = "10.10.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
   public_subnets  = ["10.10.101.0/24", "10.10.102.0/24", "10.10.103.0/24"]
 
@@ -39,14 +39,14 @@ module "my_sg" {
     {
       from_port   = 80
       to_port     = 80
-      protocol    = "http"
+      protocol    = "tcp"
       description = "Opened web server ports"
       cidr_blocks = module.my_vpc.vpc_cidr_block
     },
     {
       from_port   = 22
       to_port     = 22
-      protocol    = "ssh"
+      protocol    = "tcp"
       description = "ssh port"
       cidr_blocks = module.my_vpc.vpc_cidr_block
     }
@@ -89,5 +89,5 @@ module "my_asg" {
   image_id          = data.aws_ami.amazon-linux-image.id
   instance_type     = "t3.micro"
   ebs_optimized     = true
-  enable_monitoring = true
+  enable_monitoring = false
 }
