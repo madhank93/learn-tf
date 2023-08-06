da4964da434a8839fe663000c345f950ca829c52
Madhan <madhankumaravelu93@gmail.com>
2023-08-06 11:29:10 -0500

    feat: tf state files in s3 with state locking to avoid race condition

---
+++ aws/s3-storage-with-state-locking/Readme.md
@@ -0,0 +1,3 @@
+### Storing Terraform state files in S3 backend with locking
+
+- Create a `S3 bucket` in AWS & create a `table in Dynamo DB` and provide those information in tf files.
---
+++ aws/s3-storage-with-state-locking/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.11.0"
+    }
+  }
+  backend "s3" {
+    encrypt = true
+    bucket = "madhank93-learn-tf"
+    dynamodb_table = "tf-state-lock"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
