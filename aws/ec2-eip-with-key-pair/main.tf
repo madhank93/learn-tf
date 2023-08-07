09b2e8a5c7c3075dcaff7d8c00356fa5f145e445
Madhan <madhankumaravelu93@gmail.com>
2023-08-06 19:26:01 -0500

    feat: creating ec2 instances with nginx installed and assigning elastic ip using key pair

---
+++ aws/ec2-eip-with-key-pair/main.tf
@@ -0,0 +1,20 @@
+
+resource "aws_instance" "lunar" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name = aws_key_pair.lunar-key.key_name
+  user_data = file("install-nginx.sh")
+}
+
+resource "aws_eip" "eip" {
+  instance = aws_instance.lunar.id
+  domain = "vpc"
+  provisioner "local-exec" {
+    command = "echo ${aws_eip.eip.public_dns} >> /root/lunar_public_dns.txt"
+  }
+}
+
+resource "aws_key_pair" "lunar-key" {
+  key_name = "lunar"
+  public_key = file("~/.ssh/id_ed25519.pub")
+}
---
+++ aws/ec2-eip-with-key-pair/variables.tf
@@ -0,0 +1,14 @@
+variable "ami" {
+  default = "ami-06178cf087598769c"
+  type = string
+}
+
+variable "instance_type" {
+  default = "m5.large"
+  type = string
+}
+
+variable "region" {
+  default = "eu-west-2"
+  type = string
+}
